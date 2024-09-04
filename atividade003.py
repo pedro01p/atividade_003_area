795eaa6f6dffd635b3d03fa8dd25eede8ef86624
pedro01p <tsukki237@gmail.com>
2024-09-04 15:54:12 -0300

    calculando are

---
+++ atividade003.py
@@ -0,0 +1,7 @@
+#Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+l = float(input("digite o valor da largura"))
+a = float(input("digite o valor da altura"))
+
+multi=(l*a)
+
+print(f"a area é {multi}")
